6d3589051874c48fc04d6fcae8531a888d051326
tunahan_kgokce <tunahan_gokce_06@hotmail.com>
2025-10-23 15:38:27 +0300

    Patrolling Logic for Enemy AI

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private NavMeshAgent agent;
+    public NavMeshAgent Agent { get => agent; }
+    //for debug
+    [SerializeField]
+    private string currentState;
+    public EnemyPath path;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        stateMachine.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
